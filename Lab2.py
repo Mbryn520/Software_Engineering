5ee974fe4a1fb798ffc2aefeb6da309461d1074f
Mbryn520 <uinfasgor@gmail.com>
2025-09-21 11:06:57 +0500

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,99 @@
+# Лаб1
+print(123)
+print('123')
+print(1.23)
+
+# Лаб2
+print(1823 - 486)
+print(5.1 + 8.27)
+print(3 + 7.04 + 1 + 2.33)
+
+# Лаб3
+print("Привет, Мир!")
+
+world = 'Мир'
+print(f"Привет, {world}!")
+
+one = 'Привет, '
+two = 'Мир!'
+print(one + two)
+
+# Лаб4
+one = 'Hello'
+print(bool(one))
+
+two = 142
+print(float(two))
+
+three = None
+print(str(three))
+
+# Лаб5
+one = input('one:')
+two = input('two:')
+three = input('three:')
+print(one, two, three)
+
+# Лаб6
+a = 12
+b = 5
+print('Возведение в степень:', a ** b)
+print('Обычное деление:', a / b)
+print('Целочисленное деление:', a // b)
+print('Нахождение остатка от деления:', a % b)
+
+# Лаб7
+line = 'Hello!'
+print(line * 6)
+
+# Лаб8
+sentence = 'Hello World'
+print(sentence.count('o'))
+
+# Лаб9
+print('Hello\nWorld')
+
+# Лаб10
+sentence = 'Hello World'
+print(sentence[1])
+print(sentence[:5])
+
+# Сам1
+print(10>589)
+
+#Сам2
+a, b, c = 52, 65, 78
+print(a, b, c)
+
+#Сам3
+a = int(input())
+
+#Сам4
+strk = "Cat";
+print(strk*6)
+
+#Сам5
+day, month, year = 21, "сентября", 2025;
+print(f"Сегодня {day} {month} {year} год.", end=" Всего хорошего!")
+
+#Сам6
+strk = 'my'
+print(f'Hello {strk} World')
+
+#Сам7
+strk = 'Hello World'
+print(len(strk))
+
+#Сам8
+strk = 'HELLO WORLD'
+print(strk.lower())
+
+#Сам9
+age = int(input("Введите ваш возраст: "))
+yearnow = 2025
+youryear = yearnow - age
+print(f"Ваш год рождения: {youryear}")
+
+#Сам10
+name, city, animal = input("Ваше имя: "), input("Ваш город: "), input("Ваше любимое животное: ")
+print(f"Привет, {name}! {city} очень красивый город! {animal} - мое любимое животное тоже.")
